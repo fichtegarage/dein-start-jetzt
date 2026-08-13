397426a4a9fcdd806b82f8d8a5d7948a0dc59f9a
fichtegarage <136880823+fichtegarage@users.noreply.github.com>
2026-08-13 21:08:44 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ScrollToHash from "./components/ScrollToHash";
 import Index from "./pages/Index";
 import Start from "./pages/Start";
 import Impressum from "./pages/Impressum";
@@ -19,6 +20,7 @@ const App = () => (
         <Toaster />
         <Sonner />
         <BrowserRouter>
+          <ScrollToHash />
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/start" element={<Start />} />
